009eb48a9cd268ffe62bf7d7c6cc8b2b10897b42
Tonycrz <tonybaidoo0@gmail.com>
2021-08-26 15:06:58 +0000

    add C program that prints some text (fixed betty style)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
