338a7224aab4509eae662e0641715a61b27065f7
Tonycrz <tonybaidoo0@gmail.com>
2021-08-30 20:30:20 +0000

    prints all two two-digit combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints  all possible two two-digit combinations
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+
+		for (b = '0'; b <= '8'; b++)
+		{
+
+			for (c = '0'; c <= '9'; c++)
+			{
+
+				for (d = '1'; d <= '9'; d++)
+				{
+					if (a <= c && b < d)
+					{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+
+						if (a != '9' || b != '8' || c != '9' || d != '9')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
