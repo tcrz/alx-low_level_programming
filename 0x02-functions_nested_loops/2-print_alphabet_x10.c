5e8603b69a09c09278c47aeca66db59eb713f71d
Tonycrz <tonybaidoo0@gmail.com>
2021-08-31 14:10:08 +0000

    prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "putchar.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times, followed by a new line
+ *
+ */
+
+
+
+void print_alphabet_x10(void)
+{
+	 char x;
+	 int count = 0;
+	while (count < 10)
+	{
+		for (x = 97; x <= 122; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+		count++;
+	}
+
+
+
+}
+
