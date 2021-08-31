a125979cd9f28495949c6026b064c08233592e85
Tonycrz <tonybaidoo0@gmail.com>
2021-08-31 13:06:11 +0000

    add files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints 'Holberton' followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+	for (x = 95; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
