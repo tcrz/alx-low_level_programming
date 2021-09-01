016294eb5aa7e2d4709fdd2552fe42c042430037
Tonycrz <tonybaidoo0@gmail.com>
2021-09-01 14:20:36 +0000

    find sum of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds given integers.
+ * @a: takes an int number
+ * @b: takes an int number
+ * Return: sum of a and b if successful
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
