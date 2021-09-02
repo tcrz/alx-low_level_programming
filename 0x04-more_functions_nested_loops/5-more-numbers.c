6366cc801e583b333a260ed0cc297bb95aa55220
Tonycrz <tonybaidoo0@gmail.com>
2021-09-02 19:41:11 +0000

    print 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more-numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14.
+ *
+ */
+
+void more_numbers(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+
+			if (b > 9)
+			{
+				putchar((b / 10) + '0');
+			}
+
+			putchar((b % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
+
