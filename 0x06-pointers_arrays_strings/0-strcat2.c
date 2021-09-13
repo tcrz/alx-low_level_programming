1114e05d73f338140e3be98ef8b6fa8ac0cd007f
Tonycrz <tonybaidoo0@gmail.com>
2021-09-13 11:44:15 +0000

    change filename

---
+++ 0x06-pointers_arrays_strings/0-strcat2.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: Destination string
+ * @src: String for adding
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, k, num;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	j = 0;
+	while (src[j] != '\0')
+		j++;
+
+	if (i < j)
+		num = j;
+	else if (i >= j)
+		num = i;
+
+	for (k = 0; k < num; k++)
+	{
+		if (num == j)
+			dest[k + num - 1] = src[k];
+		else
+			dest[k + num] = src[k];
+	}
+
+	return (dest);
+}
