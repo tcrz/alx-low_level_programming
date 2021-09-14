9b59e373dd4af48c00708b49ee5d42364ae3b226
Tonycrz <tonybaidoo0@gmail.com>
2021-09-14 10:39:45 +0000

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	while ((*s1 && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-
-	if (i == j)
+	if (*s1 == *s2)
 		return (0);
 	else
-		return (i - j);
+		return (*s1 - *s2);
 }
+
