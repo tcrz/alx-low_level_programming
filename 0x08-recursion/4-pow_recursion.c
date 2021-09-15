b27ebb725f367f0ada2da331d40f217ababba1d5
Tonycrz <tonybaidoo0@gmail.com>
2021-09-15 17:52:37 +0000

    returns of x raised to the power y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,12 +9,14 @@
 
 int _pow_recursion(int x, int y)
 {
+	int power;
+
 	if (y == 0)
 		return (1);
 	else if (y < 0)
 		return (-1);
 
-	int power;
+
 	 power = x * _pow_recursion(x, (y - 1));
 	return (power);
 }
