35533b9625aaba1f161f8c15fd32dddd1be6fd08
Tonycrz <tonybaidoo0@gmail.com>
2021-09-23 21:03:47 +0000

    add

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 
-	length = i + n;
+	length = i + n + 1;
 
 	concat = malloc(sizeof(char) * length);
 	if (concat == NULL)
@@ -32,11 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		concat[i] = s1[i];
 
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && s2[j] != '\0'; j++)
 		concat[i + j] = s2[j];
 
 	concat[i + j] = '\0';
 	return (concat);
 
 }
-
