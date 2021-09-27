853d1164c85da365bbcaff96d7a566794d517fb1
Tonycrz <tonybaidoo0@gmail.com>
2021-09-27 14:07:11 +0000

    Absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,15 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ({\
+#define ABS(x) (abs(x))
+int abs(int x)
+{
 	if (x < 0)
-	x *= -1;
-})
+	{
+		x *= -1;
+	}
 
+	return (x);
+}
 
 #endif
