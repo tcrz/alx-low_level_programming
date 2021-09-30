5767f2c58e46134ebe6fa6316f11907e0e5ee829
Tonycrz <tonybaidoo0@gmail.com>
2021-09-30 18:11:00 +0000

    prints string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,6 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		char *str = va_arg(ptr, char*);
+
 		if (separator == NULL)
 			separator = "";
 
