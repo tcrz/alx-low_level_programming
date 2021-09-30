0c62008796609dd3786918d8e0640b72937101c9
Tonycrz <tonybaidoo0@gmail.com>
2021-09-30 16:40:06 +0000

    sum of numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,15 +9,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum, i;
+	unsigned int sum = 0, i;
 	va_list ptr;
 
 	if (n == 0)
 		return (0);
 
 	va_start(ptr, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
+
+	va_end(ptr);
 
 	return (sum);
 }
