a8efeb29e59d057de1c55fd9e1714bf6d9d7b029
Tonycrz <tonybaidoo0@gmail.com>
2021-10-07 20:26:39 +0000

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	list_t *new_node;
 
-	if(head == NULL)
+	if (head == NULL || str == NULL)
 		return (NULL);
 
 	new_node = (list_t *) malloc(sizeof(list_t));
