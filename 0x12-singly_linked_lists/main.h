1934b27708c9fc84981f8153cc6dad034e6d8bd6
Tonycrz <tonybaidoo0@gmail.com>
2021-10-07 14:24:46 +0000

    header file

---
+++ 0x12-singly_linked_lists/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+
+
+
+
+#endif
