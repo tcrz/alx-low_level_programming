69710374cd260db773f8a7097af404f7cf0affe8
Tonycrz <tonybaidoo0@gmail.com>
2021-10-07 20:35:30 +0000

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,15 +12,20 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 
 	list_t *new_node;
+	int len;
 
 	new_node = (list_t *) malloc(sizeof(list_t));
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	new_node->len = len;
 
 	head = &new_node;
 
 	if (head == NULL)
-                return (NULL);
+		return (NULL);
 
 	printf("[%u] %s\n", new_node->len, new_node->str);
 	return (new_node);
