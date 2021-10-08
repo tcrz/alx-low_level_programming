381797328d31d70e681c85ddf87d43f97abf0421
Tonycrz <tonybaidoo0@gmail.com>
2021-10-08 13:28:54 +0000

    f r e e

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,13 +4,13 @@
  * free_list -  frees a list_t list
  *
  * @head: pointer to struct list_t
- * @str: string
 */
 
 void free_list(list_t *head)
 {
 	list_t *current = head;
-	while(current)
+
+	while (current)
 	{
 		free(current->str);
 		free(current);
