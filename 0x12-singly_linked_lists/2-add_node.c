63a15d7569512a3c1f4be2c46b1263dcef54c6af
Tonycrz <tonybaidoo0@gmail.com>
2021-10-11 10:56:51 +0000

    final

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 	new_node->len = len;
-	new_node->next = NULL;
+	new_node->next = *head;
 	*head = new_node;
 
 	return (*head);
