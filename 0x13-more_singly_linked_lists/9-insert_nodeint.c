739b64d8e440179d01d843b6de24464749b28740
Tonycrz <tonybaidoo0@gmail.com>
2021-10-12 18:35:42 +0000

    insert node at nth

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,6 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	if (idx == 0)
 	{
+		new_node->next = *head;
 		(*head) = new_node;
 		return (new_node);
 	}
@@ -38,6 +39,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	current->next = new_node;
 	new_node->next = temp;
 
-	return (new_node);
+	return (new_node ? new_node : NULL);
 
 }
