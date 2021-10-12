fe9c711da45b2cad1283cb24a77e50e52e29642c
Tonycrz <tonybaidoo0@gmail.com>
2021-10-12 18:30:23 +0000

    insert node at nth

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+
+/**
+ * insert_nodeint_at_index -  frees a listint_t list
+ *
+ * @head: pointer to a pointer of type listint_t
+ * @idx: index of the list where new node should be added.
+ * @n: data
+ * Return: address of new node if successful else NULL
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current = *head;
+	listint_t *temp;
+	listint_t *new_node;
+	unsigned int count = 1;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	if (idx == 0)
+	{
+		(*head) = new_node;
+		return (new_node);
+	}
+
+	while (current && idx > count)
+	{
+		current = current->next;
+		count++;
+	}
+
+	temp = current->next;
+	current->next = new_node;
+	new_node->next = temp;
+
+	return (new_node);
+
+}
