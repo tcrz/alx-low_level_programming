26199d7502085ef87c528e3905371b1f92172aea
Tony <81537498+tcrz@users.noreply.github.com>
2021-10-15 18:22:33 +0000

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,12 +11,13 @@ unsigned int binary_to_uint(const char *b)
         int i, len, x;
         unsigned int total = 0;
 
+        if (!b)
+                return (0);
+        
         len = 0;
         while (b[len])
                 len++;
-        else
-                return (0);
-
+        
         len = len - 1;
         for (i = 0; b[i]; i++, len--)
         {
