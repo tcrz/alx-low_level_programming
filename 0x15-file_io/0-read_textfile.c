fee615b8e2920ad0878072dbfaf82883a57f0ae9
Tonycrz <tonybaidoo0@gmail.com>
2021-10-18 17:03:55 +0000

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,47 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: file name
- * @letters: number of letters it should read and print
- * Return: actual number of letters it should read/print else 0
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: the number of letters printed, or 0 if it failed
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int x, y;
-	char *c;
+	int s, t;
+	char *buf;
 
 	if (!filename)
 		return (0);
 
-	 fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
 
-	c = malloc(sizeof(char) * letters);
-	if (!c)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
 
-	x = read(fd, c, letters);
-	if (x < 0)
+	s = read(fd, buf, letters);
+	if (s < 0)
 	{
-		free(c);
+		free(buf);
 		return (0);
 	}
-	c[x] = '\0';
+	buf[s] = '\0';
 
 	close(fd);
 
-	y = write(STDOUT_FILENO, c, x);
-	if (y < 0)
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
 	{
-		free(c);
+		free(buf);
 		return (0);
 	}
 
-	free(c);
-	return (y);
+	free(buf);
+	return (t);
 }
+
