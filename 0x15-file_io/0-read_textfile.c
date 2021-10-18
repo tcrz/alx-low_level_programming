5cb32e050c2aae189e88e5ed324c1834e9464014
Tonycrz <tonybaidoo0@gmail.com>
2021-10-18 16:44:57 +0000

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if(fd < 0)
+	if (fd < 0)
 		return (0);
 
 	x = read(fd, c, letters);
