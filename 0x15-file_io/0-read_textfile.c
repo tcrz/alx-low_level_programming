73fa365a34e2e887639b7c71bd7cdcbcf70290ef
Tonycrz <tonybaidoo0@gmail.com>
2021-10-18 16:47:33 +0000

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,12 +16,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 
+	 fd = open(filename, O_RDONLY);
+        if (fd < 0)
+                return (0);
+
 	c = malloc(sizeof(char) * letters);
 	if (!c)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
 		return (0);
 
 	x = read(fd, c, letters);
