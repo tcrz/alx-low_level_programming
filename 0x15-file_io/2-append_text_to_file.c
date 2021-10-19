15c4bc4318741b1a84496ceb9760efda4aa9475f
Tonycrz <tonybaidoo0@gmail.com>
2021-10-19 11:06:59 +0000

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while(text_content[len])
+		while (text_content[len])
 			len++;
 	}
 	sz = write(fd, text_content, len);
