884fc843d3bbd4660608658dd39ebcfdea92ac9a
Tonycrz <tonybaidoo0@gmail.com>
2021-10-19 10:32:12 +0000

    change approach

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	while (text_content[len])
+	if (text_content)
+	{
+		while (text_content[len])
 		len++;
+	}
 
 	sz = write(fd, text_content, len);
 	if (sz < 0)
