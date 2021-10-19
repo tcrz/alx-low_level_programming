33249c074a96eac5aa4f8c58185a6b2b87b9e171
Tonycrz <tonybaidoo0@gmail.com>
2021-10-19 15:17:11 +0000

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
 int main(int ac, char **av)
 {
 	int fd1, fd2, sz1, a, b;
-	char buf[2000];
+	char buf[BUFSIZ];
 
 	if (ac != 3)
 	{
@@ -24,7 +24,7 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((sz1 = read(fd1, buf, 2000)) > 0)
+	while ((sz1 = read(fd1, buf, BUFSIZE)) > 0)
 	{
 		if (fd2 < 0 || write(fd2, buf, sz1) != sz1)
 		{
