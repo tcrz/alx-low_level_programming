b227524e9c50234d1f4b3a2c8a403933bf6f7d0a
Tonycrz <tonybaidoo0@gmail.com>
2021-10-19 22:08:21 +0000

    fix update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,51 +2,50 @@
 
 /**
  * main - copies the content of a file to another file
- * @argc: number of arguments passed to the program
- * @argv: pointer to array of arguments
+ * @ac: number of arguments passed to the program
+ * @av: pointer to array of arguments
  *
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
-	int fd_r, fd_w, r, a, b;
-	char buf[BUFSIZ];
+	int fd1, fd2, sz;
+	char buf[1024];
 
-	if (argc != 3)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_r = open(argv[1], O_RDONLY);
-	if (fd_r < 0)
+	fd1 = open(av[1], O_RDONLY);
+	if (fd1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	while ((sz = read(fd1, buf, 1024)) > 0)
 	{
-		if (fd_w < 0 || write(fd_w, buf, r) != r)
+		if (fd2 < 0 || write(fd2, buf, sz) != sz)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_r);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
-	if (r < 0)
+	if (sz < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
 		exit(98);
 	}
-	a = close(fd_r);
-	b = close(fd_w);
-	if (a < 0 || b < 0)
+	if (close(fd1) < 0)
 	{
-		if (a < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
-		if (b < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);
