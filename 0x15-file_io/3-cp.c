acf6d1cbf1b39c2e465d9c5b6a0cd8581133b3f3
Tonycrz <tonybaidoo0@gmail.com>
2021-10-19 17:02:52 +0000

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,18 +10,18 @@
 
 int main(int ac, char **av)
 {
-	int fd1, fd2, sz;
+	int fd1, fd2, sz, a, b;
 	char buf[1024];
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage:cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage:cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -29,25 +29,36 @@ int main(int ac, char **av)
 	{
 		if (fd2 < 0 || write(fd2, buf, sz) != sz)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write from file %s", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", av[2]);
 			close(fd1);
 			exit(99);
 		}
 	}
 	if (fd2 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write from file %s", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", av[2]);
 		exit(98);
 	}
-	if (close(fd1) < 0)
+	a = close(fd1);
+	b = close(fd2);
+	if (a < 0 || b < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %d", close(fd1));
+		if (a < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		if (b < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+
+	/*if (close(fd1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", close(fd1));
 		exit(100);
 	}
 	if (close(fd2) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %d", close(fd2));
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", close(fd2));
 		exit(100);
-	}
+	} */
 	return (0);
 }
