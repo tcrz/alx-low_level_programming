4f2e53e1a6ed7bb63039a667d664874b9175cf08
Tonycrz <tonybaidoo0@gmail.com>
2021-11-25 12:46:45 +0000

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,7 +26,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 #endif
