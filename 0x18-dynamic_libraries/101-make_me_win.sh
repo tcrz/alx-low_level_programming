56cfb428b72e20eadf4f0d8a06d90597561984b5
Tonycrz <tonybaidoo0@gmail.com>
2021-11-29 16:30:32 +0000

    updates

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ./ https://github.com/tcrz/alx-low_level_programming/blob/master/0x18-dynamic_libraries/injection.so
+wget -P https://github.com/tcrz/alx-low_level_programming/blob/master/0x18-dynamic_libraries/injection.so
 export LD_PRELOAD=./injection.so
