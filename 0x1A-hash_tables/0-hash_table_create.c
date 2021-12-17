da931c99a03e38d85af6e021a5dca886f21c3af0
Tonycrz <tonybaidoo0@gmail.com>
2021-12-17 16:43:28 +0000

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,9 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t  *hash_table = malloc(sizeof(hash_table_t);
+	hash_table_t  *hash_table = NULL;
+
+	hash_table =  malloc(sizeof(hash_table_t));
 
 	if (!hash_table)
 		return (NULL);
