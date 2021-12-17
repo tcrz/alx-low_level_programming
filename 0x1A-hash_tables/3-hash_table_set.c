013e3fc2a9b0bd69b342c825e0b2335d4ab4aa02
Tonycrz <tonybaidoo0@gmail.com>
2021-12-17 11:08:02 +0000

    update return value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (!entry->value)
 				return (0);
 			entry->value = strdup(value);
-			return (21);
+			return (1);
 		}
 		entry = entry->next;
 	}
