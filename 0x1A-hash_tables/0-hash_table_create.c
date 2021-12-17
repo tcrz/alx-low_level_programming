45961c181b84262eba48e3ae9b3d6f397d569a1c
Tonycrz <tonybaidoo0@gmail.com>
2021-12-17 08:35:45 +0000

    betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t  *hash_table = malloc(sizeof(hash_table_t));
+
 	if (!hash_table)
 		return (NULL);
 	if (!size)
