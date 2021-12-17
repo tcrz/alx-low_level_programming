a150a8bf7aeece4ee52ceb94dcb8cee37383c851
Tonycrz <tonybaidoo0@gmail.com>
2021-12-17 13:12:06 +0000

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,9 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *entry;
 	int idx;
 
-	if (!ht)
-		return (NULL);
-	if (strlen(key) == 0)
+	if (!ht || !key || strlen(key) == 0)
 		return (NULL);
 	idx = key_index((const unsigned char *)key, ht->size);
 	entry = ht->array[idx];
